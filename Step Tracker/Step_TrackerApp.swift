15c49234d643cfe35930964550d3e18c7b600412
Leonard-McCook <leonard.mccookcarr@outlook.com>
2024-05-10 18:13:01 -0400

    Created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Leonard McCook-Carr on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
